7f958f394e88fa5a22af387624a3b94e1fd4dea8
xeruf <27jf@pm.me>
2022-09-10 00:38:57 +0200

    chore: fix windows setup inconveniences

--- i.ps1
+++ i.ps1
@@ -1,6 +1,6 @@
 # manual execution: Set-ExecutionPolicy Bypass -Scope Process -Force; .\i
-(Test-Path "$PWD\instalee") -and (cd instalee)
+(Test-Path "$PWD\instalee" -PathType Container) -and (cd instalee) | out-null
 packages\chocolatey\powershell.ps1
 Start-Process -Wait choco -Verb runAs -ArgumentList 'install git'
 Start-Process -Wait "C:\Program Files\Git\git-bash.exe" -Verb runAs -ArgumentList "-c './instalee win/office; sleep 10 || bash'"
-Sleep 10
+# Sleep 10
